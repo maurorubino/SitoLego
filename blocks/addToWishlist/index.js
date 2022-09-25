c9d9f73b0aaf9a3f73197abb8f2285b809002b94
Filippo Scelfo <scelfofilippo@gmail.com>
2022-09-25 20:19:48 +0200

    Created add to fav component interaction for mobile

--- blocks/addToWishlist/index.js
+++
@@ -1,9 +0,0 @@
-function addToWishlist() {
-  var element = document.getElementById("addToWishlist");
-  element.classList.add("liked");
-}
-
-function removeToWishlist() {
-  var element = document.getElementById("addToWishlist");
-  element.classList.remove("liked");
-}
